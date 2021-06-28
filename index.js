e332075609276bf8536ab22fbc5e33dddd34fe28
Josu Igoa <josuigoa@gmail.com>
2021-06-28 12:08:45 +0200

    added onLongPress

--- index.js
+++ index.js
@@ -3,27 +3,27 @@ class RandomColor {
 	setup(props, server) {
 		this.props = props == null ? {} : props;
 		this.server = server;
-        
-        if (this.props == null) {
-            this.props = {};
-        }
-        
-        if (this.props.setText == null) {
-            this.props.setText = true;
-        }
+		
+		if (this.props == null) {
+			this.props = {};
+		}
+		
+		if (this.props.setText == null) {
+			this.props.setText = true;
+		}
 	}
 	
 	init() {
 	}
-    
-    getRandomColorHex() {
-        const colorLetters = '0123456789ABCDEF';
-        let color = '';
-        for (let i = 0; i < 6; i++) {
-            color += colorLetters[Math.floor(Math.random() * 16)];
-        }
-        return color;
-    }
+	
+	getRandomColorHex() {
+		const colorLetters = '0123456789ABCDEF';
+		let color = '';
+		for (let i = 0; i < 6; i++) {
+			color += colorLetters[Math.floor(Math.random() * 16)];
+		}
+		return color;
+	}
 	
 	execute(currentState) {
 		return new Promise((resolve, _) => {
@@ -32,6 +32,12 @@ class RandomColor {
 				currentState.text = '#' + color;
 			}
 			currentState.bgColor = 'ff' + color;
+			resolve(currentState);
+		});
+	}
+
+	onLongPress(currentState) {
+		return new Promise(function (resolve, _) {
 			resolve(currentState);
 		});
 	}
